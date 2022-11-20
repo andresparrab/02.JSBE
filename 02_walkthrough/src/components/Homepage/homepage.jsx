e0429f0783a975b623c33e3830135e2d5f9afdc6
andresparrab <andres.parrab@gmail.com>
2022-11-20 17:46:53 +0100

    refreshing some code

--- 02_walkthrough/src/components/Homepage/homepage.jsx
+++ 02_walkthrough/src/components/Homepage/homepage.jsx
@@ -5,7 +5,7 @@ function homepage()
   return (
   <div className="image">
       <img src="https://m.media-amazon.com/images/I/41J33D8T64L.jpg" alt="" />
-      <h1 style={{color: "red", fontSize: "30px", display: "flex", alignItems: "center", justifyContent: "center"}} >book</h1>
+      <h1 className="text" style={{color: "red", fontSize: "30px", display: "flex", alignItems: "center", justifyContent: "center"}} >book</h1>
     </div>
   )
 }
