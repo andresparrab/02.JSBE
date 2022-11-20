0d29ec7db66281abc1eeba8877e9c50e92261f7f
andresparrab <andres.parrab@gmail.com>
2022-11-20 09:39:13 +0100

    trying to do a portfiol with links

---
+++ 03.portfolio/src/components/Nav/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Menu() {
+  return (
+    <div className='Menu-container'>
+      <ul>
+        <li> <Link to='/'>Link</Link>
+        </li>
+      </ul>
+
+    </div>
+
+  )
+}
+
+export default Menu
--- 03.portfolio/src/components/Nav/Nav.jsx
+++ 03.portfolio/src/components/Nav/Nav.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './nav.css';
+import Menu from './Menu/Menu';
+
+
 import Headroom from "react-headroom"
 function Nav() {
   return (
     <Headroom>
       <header>
-        
-    <div className='Nav-container' >
-      <span className='code'>&lt;</span>   
-      <span className='logo-name'>{'Andres'}</span>   
-      <span className='code'>/&gt;</span>   
+        <div className='Nav-container' >
+          <span className='code'>&lt;</span>
+          <span className='logo-name'>{'Andres'}</span>
+          <span className='code'>/&gt;</span>
+        </div>
+        <div className='Menu'>
+          <Menu />
 
         </div>
       </header>
