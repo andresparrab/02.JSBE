2ae826d6dbe25bc67b225e698989178802ae827f
andresparrab <andres.parrab81@gmail.com>
2022-11-24 19:45:54 +0100

    added a skeleton navbar

--- 03b.portfolioVim/pages/components/layouts/main.js
+++ 03b.portfolioVim/pages/components/layouts/main.js
@@ -1,6 +1,6 @@
 import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
-
+import Navbar from './navbar'
 const Main = ({ children, router }) => {
   return (
     <Box as='Main' pb={8}>
@@ -8,6 +8,7 @@ const Main = ({ children, router }) => {
         <meta name='viewport' content="width=device-width, initial-scale=1" />
         <title>Andres Parra - Homepage</title>
       </Head>
+      <Navbar />
       <Container maxW="container.md" pt={15}>
         {children}
       </Container>
---
+++ 03b.portfolioVim/pages/components/layouts/navbar.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+
+const Navbar = (router, children) => {
+  return (
+    <Box>
+      <h5>my nav bar </h5>
+    </Box>
+  )
+}
+
+export default Navbar
