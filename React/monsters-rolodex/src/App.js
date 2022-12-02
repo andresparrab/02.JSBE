f5dd869a68d7b3878b13432a15b021ef1ec0b522
andresparrab <andres.parrab81@gmail.com>
2022-12-02 06:58:32 +0100

    search logic almost done, does not update after initial successfull filtering

--- React/monsters-rolodex/src/App.js
+++ React/monsters-rolodex/src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     this.state = {
       monsters: []
     }
+    console.log("constructor")
   }
 
   componentDidMount() {
+    console.log("componentDidMount")
     fetch('https:jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) =>
@@ -22,11 +24,29 @@ class App extends Component {
           }))
   }
   render() {
+    console.log("render")
     return (
       <div className='App'>
+        <input className="search-box" type="search" placeholder="search monsters" onChange={
+          (event) => {
+            const filteredMonters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase());
+            })
+            this.setState(() => {
+              return { monsters: filteredMonters }
+
+            });
+            console.log("this is the monter", filteredMonters.map((monter) => monter.name))
+            console.log(event.target.value)
+          }} />
         {
-          this.state.monsters.map((monster) => <h1 key={monster.id}> {monster.name}</h1>)
-        }
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1> {monster.name}</h1>
+              </div>
+            )
+          })}
       </div>
     );
   }
